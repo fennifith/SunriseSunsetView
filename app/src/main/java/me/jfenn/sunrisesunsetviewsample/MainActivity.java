3457052f39400701e3efa3614c83210d88c02f6a
James Fenn <18jafenn90@gmail.com>
2018-11-15 22:49:03 -0500

    implement sample app

---
+++ app/src/main/java/me/jfenn/sunrisesunsetviewsample/MainActivity.java
@@ -0,0 +1,35 @@
+package me.jfenn.sunrisesunsetviewsample;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import androidx.appcompat.app.AppCompatActivity;
+import me.jfenn.sunrisesunsetview.SunriseSunsetView;
+
+public class MainActivity extends AppCompatActivity implements SunriseSunsetView.SunriseListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SunriseSunsetView view = findViewById(R.id.sunView);
+        view.setListener(this);
+    }
+
+    @Override
+    public void onSunriseChanged(long sunriseMillis) {
+        Toast.makeText(this, "Sunrise set to " + new SimpleDateFormat("HH:mm", Locale.getDefault())
+                .format(new Date(sunriseMillis)), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSunsetChanged(long sunsetMillis) {
+        Toast.makeText(this, "Sunset set to " + new SimpleDateFormat("HH:mm", Locale.getDefault())
+                .format(new Date(sunsetMillis)), Toast.LENGTH_SHORT).show();
+    }
+}
